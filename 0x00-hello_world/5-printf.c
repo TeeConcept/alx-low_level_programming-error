e2364293bfa8740d35e286c638c3e59a1562ad3b
TeeConcept <ibiduntosin@yahoo.com>
2022-04-07 23:55:41 -0700

    c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*My hello, print task
+*retuns 1 afer execution*/
+int main(void)
+{
+printf("with proper grammar, but the outcomeis a place of art,\n");
+return(0)
+}
+
