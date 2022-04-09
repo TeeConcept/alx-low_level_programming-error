2ec87704aae2c769f4b138d8e2ca54bd7c4f7cc5
TeeConcept <ibiduntosin@yahoo.com>
2022-04-09 15:09:18 -0700

    neorpos

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,7 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-* 
+* main - Entry point 
 * Return: Always 0 (Success)
 */
 
@@ -14,12 +13,12 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-if (n==0)
+if (n == 0)
 {
 printf("%i is zero\n", n);
 }
 
-else if (n<0)
+else if (n < 0)
 {
 printf("%i is negative\n", n);
 }
