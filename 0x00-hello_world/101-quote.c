42749f5e006077ddd26c727866839e4245f0b23a
TeeConcept <ibiduntosin@yahoo.com>
2022-04-09 04:58:54 -0700

    writest

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main - Print out the part of a quote in the stadard error.
-* Return: 1 if success.
+* main - Entry point
+* Return: Always 1 (Success)
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
