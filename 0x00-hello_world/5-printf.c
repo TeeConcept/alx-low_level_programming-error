a4d40b309f321ba68cb9b46b7d289a12bf34429f
TeeConcept <ibiduntosin@yahoo.com>
2022-04-09 02:12:29 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
-/*
- *
-*main - Entry point
-*My hello, print task
-*retuns execution
+/**
+* main - Entry point
+* My hello, print task
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-Return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
 
