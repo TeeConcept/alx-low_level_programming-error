f489846122d4027413aca7de48939d2667392ca2
TeeConcept <ibiduntosin@yahoo.com>
2022-04-10 15:51:07 -0700

    combo5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,16 @@ int main(void)
 	int i, j;
 	for (i = 0; i <= 98; i++)
 	{
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+		putchar(' ');
+		putchar((j / 10) + '0');
+		putchar((j % 10) + '0');
+
 		for (j = i + 1; j <= 99; j++)
 			if (i == 98 && j == 99)
 				continue;
+
 		putchar(',');
 		putchar(' ');
 
