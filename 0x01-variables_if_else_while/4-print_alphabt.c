99ebf18739693c5af1fc2e05b4f8aec53ec998aa
TeeConcept <ibiduntosin@yahoo.com>
2022-04-10 11:55:23 -0700

    exempt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char aphabt;
+for (alphabt = 'a'; alphabt <= 'z'; alphabt++)
+{
+if (alphabt == 'q')
+continue;
+
+else if (alphabt == 'e')
+continue;
+
+putchar(aphabt);
+}
+
+putchar('\n');
+return (0);
+}
