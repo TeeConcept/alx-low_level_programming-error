99fc7431368ba4770482c41d5b6bffbf27425aa7
TeeConcept <ibiduntosin@yahoo.com>
2022-04-10 13:39:37 -0700

    combo4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i, j, k;
+for (i = 2; i < 12; i++)
+{
+for (j = 3; j < 12; j++)
+{
+for (k = 4; k < 12; k++)
+{
+if (k > j && j > i)
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if (i != 5 || j != 6)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
