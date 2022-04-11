96bd23311770a95094289d473bf3f20c7bcc06ce
TeeConcept <ibiduntosin@yahoo.com>
2022-04-11 05:33:31 -0700

    combo4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,27 +6,21 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-{
-int i, j, k;
-for (i = 0; i < 10; i++)
-{
-for (j = 1; j < 10; j++)
-{
-for (k = 2; k < 10; k++)
-{
-if (k > j && j > i)
-{
 
-putchar(i);
-putchar(j);
-putchar(k);
-
-if (i == '7' && j == '8' && k == '9')
-	continue;
-
+int digit1, digit2, digit3;
+for (digit1 = 0; digit1 < 8; digit1++)
+{
+for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+{
+for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+{
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
+putchar((digit3 % 10) + '0');
+if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+continue;
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
